5dc4f3b6fd77fba8b1daa540dfcb2398edcaa7ac
Aditya Bagga <aditya.bagga.ynr@gmail.com>
2026-01-04 22:18:42 +0000

    datatypes done

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS as newer version
+
+// alert(3+3) we are using nodejs not browser
+
+console.log(3 + 3);
+//code readability should be preferred
+console.log("Aditya") 
+
+let name = "Aditya"
+
+let age = 23
+
+let isLoggedIn = false
+
+//number
+//bigint
+//string=""
+//boolean = true,false
+//null(standalone value) empty value representation (object)
+//undefined let state;
+//symbol=> unique
+
+
+// object 
+
+console.log(typeof "adi ")
+console.log(typeof age)
+console.log(typeof null) // object
+console.log(typeof undefined)//undefined
