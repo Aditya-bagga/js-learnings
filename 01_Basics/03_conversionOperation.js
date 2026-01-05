759175d2c3c5b47cbdf1a875c366a78fe4f9a6dc
Aditya Bagga <aditya.bagga.ynr@gmail.com>
2026-01-05 22:42:21 +0000

    typeConversion

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+// let score = "33abcv" -> NaN
+// let score = null -> 0
+// let score = undefined -> NaN
+// let score = true -> 1 , false ->0
+// let score = "aditya" -> Nan
+// let score = "33" -> 33
+
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber  = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+
+
+
+// let isLoggedIn = 1 -> true , 0-> false
+// let isLoggedIn = "" -> false
+//let isLoggedIn = "Aditya" -> true
+
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof(booleanIsLoggedIn))
+// console.log(booleanIsLoggedIn)
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof(stringNumber))
+console.log(stringNumber)
