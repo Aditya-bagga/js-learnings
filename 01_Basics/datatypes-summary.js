79aada2ad34ead42d834882988ebe38005e84dca
Aditya Bagga <aditya.bagga.ynr@gmail.com>
2026-01-07 19:10:45 +0000

    datatypes and memory done

--- 01_Basics/datatypes-summary.js
+++ 01_Basics/datatypes-summary.js
@@ -26,20 +26,47 @@ const bigNumber = 345673436563423422332n //add n now it is bigInt
 // Array , Objects , Functions 
 
 
-const heros= ["shaktiman "  ,"bheem" ,"kirmada"];  //array
-let myObj={
-    name: "Aditya",   //object
-    age: 23,
+// const heros= ["shaktiman "  ,"bheem" ,"kirmada"];  //array
+// let myObj={
+//     name: "Aditya",   //object
+//     age: 23,
+// }
+
+
+// const myFunction= function(){
+// console.log("hello world");    //function
+
+// }
+
+
+// console.log(typeof bigNumber); //bigint
+// console.log(typeof myFunction); //function object
+
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//Stack(primitie) we get a copy and changes made in only copy not in orignal
+//  Heap memory(non primitive ) we get a reference to orignal so changes made at any point 
+//will reflect in orignal
+
+let myGmail = "aditya.bagga.ynr@gmail.com"
+
+let anothername = myGmail
+
+anothername = "adi"
+console.log(myGmail);
+
+console.log(anothername);
+
+
+let user1= {
+    email:"user@google.com",
+    upi:"user@ybl"
 }
 
+let user2= user1
+user2.email = "usertwo@gmail.com"
 
-const myFunction= function(){
-console.log("hello world");    //function
-
-}
-
-
-console.log(typeof bigNumber); //bigint
-console.log(typeof myFunction); //function object
-
-
+console.log(user1.email);
+console.log(user2.email);
