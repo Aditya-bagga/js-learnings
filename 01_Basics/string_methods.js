a6504cbee5939561ae2391b6419279833348afa2
Aditya Bagga <aditya.bagga.ynr@gmail.com>
2026-01-08 04:43:29 +0000

    string methods(All)

---
+++ 01_Basics/string_methods.js
@@ -0,0 +1,22 @@
+const myName = new String("Aditya-bagga")
+
+console.log(myName);
+
+// 1) Anchor (deprecated) :  a- hyper link to other webpages and we can create this from js
+
+// 2) at() take both positive and neg value , return the element at the index provided
+
+// console.log(myName.at(2));
+// console.log(myName.at(-1));
+
+// 3) big() (deprecated) it creates an HTML string and then replaces the document's body with it:
+
+// 4) blink() (deprecated) it will create a blink tag and the text will be inserted in HTML
+
+// 5) bold() (deprecated) reates a string that embeds this string in a <b> element (<b>str</b>), which causes this string to be displayed as bold
+
+// 6) charAt() //ignores negative value 
+// let index = 4
+// console.log(`char at ${index} for ${myName} is ${myName.charAt(index)}`)
+
+// 7) 
