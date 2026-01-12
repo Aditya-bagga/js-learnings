1c0896e5e03865574350caa08c6048d377d7e1e5
Aditya Bagga <aditya.bagga.ynr@gmail.com>
2026-01-12 15:01:32 +0000

    arrays part 2

---
+++ 02_Basics/02_arrays.js
@@ -0,0 +1,9 @@
+const marvel_heros = ["thor" ,"Ironman" ,"spiderman"]
+const dc_heros = ["superman" , "flash" ,"batman"]
+
+// marvel_heros.push(dc_heros) //array inside array (can push any data)
+// console.log(marvel_heros) 
+// console.log(marvel_heros[3][1]) // accessing array inside a array
+
+marvel_heros.concat(dc_heros)
+console.log(marvel_heros);
