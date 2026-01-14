bd321478550ca73fc0d000c8bbf4279311601732
Aditya Bagga <aditya.bagga.ynr@gmail.com>
2026-01-14 17:19:11 +0000

    scopes

---
+++ 03_Basics/02_scopes.js
@@ -0,0 +1,14 @@
+// //global scope
+ 
+// if(true){
+// let a = 10
+// const b =20. //block scope
+// var c = 30
+// }
+
+// // console.log(a); //a and b not accessible
+// // console.log(b);
+// console.log(c);  ->30
+
+
+                                                                                                    
