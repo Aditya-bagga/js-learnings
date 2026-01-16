133b1b14b7dea76a3efa4bfe20f90492c9b6f621
Aditya Bagga <aditya.bagga.ynr@gmail.com>
2026-01-16 09:10:39 +0000

    for in done

---
+++ 05_loops/04_forof.js
@@ -0,0 +1,41 @@
+const myObject = {
+    js: "javascript",
+    cpp : "C++",
+    rb : "ruby",
+    swift:"swift by apple"
+}
+
+// for in .....
+
+// for (const key in object){
+
+// }
+
+for (const key in myObject){
+    // console.log(key); // all keys...
+    // console.log(myObject[key]); // value
+    
+    
+}
+
+
+const programming = ['js' , ' cpp' , "ruby" ,"java"]
+
+for (const key in programming){
+    // console.log(key); // it retun keyss , for of give value directly
+    // console.log(programming[key]);
+    
+}
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United states of america")
+// map.set('fr' , "france")
+
+
+// for (const key in map){
+//     console.log(key);  //maps not iterable 
+    
+// }
+
+
