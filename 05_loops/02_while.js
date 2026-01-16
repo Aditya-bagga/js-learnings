bfdb5cfa561e785bcd8bd0246b10392a20cefba7
Aditya Bagga <aditya.bagga.ynr@gmail.com>
2026-01-16 08:39:31 +0000

    while loop done

---
+++ 05_loops/02_while.js
@@ -0,0 +1,35 @@
+//while
+
+// while (condition) {
+    //loop works till condition remain true
+// }
+
+
+let index = 0
+while(index <=10){
+    // console.log(`value of index is ${index}` );
+    index+=2;
+    
+}
+
+
+let myArray = ["flash" ,"superman" ,"batman"]
+let arr= 0
+while(arr<myArray.length){
+    // console.log(`value is ${myArray[arr]}`);
+    arr+=1;
+    
+}
+
+//do while loop
+
+// do{
+
+// }while(condition);
+
+
+let score =12;
+do{
+    console.log(`score is ${score}`);
+    score+=1
+}while(score<=10);
