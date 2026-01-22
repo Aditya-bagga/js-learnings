f4db0c7ccb5d4283ed1eeb5705abfee9c5837ec7
Aditya Bagga <aditya.bagga.ynr@gmail.com>
2026-01-22 15:17:11 +0000

    prototypes completed

--- 06_prototypes/prototype_2.js
+++ 06_prototypes/prototype_2.js
@@ -68,10 +68,17 @@ const obj1 = {
 const obj2 = {
     fname : "divya",
     lname : "bagga",
-    getFullname : function(){
-        return `${this.fname} ${this.lname}`
-    }
+    // getFullname : function(){
+    //     return `${this.fname} ${this.lname}`
+    // }
+    //__proto__ :Object
 }
+//obj1.__proto__.__proto__=null
+obj2.__proto__ = obj1;  //prototype inheritance....//chaining. // shouldnt be done this way...
+// console.log(obj2.getFullname())
+// console.log(obj2.toString())
+
+
 // console.log(obj1);
 // console.log(obj1.getFullname());
 
@@ -92,9 +99,73 @@ class Person {
 
 const p1 = new Person('Aditya' , 'Bagga')
 const p2 = new Person('Guddu' , 'Budhu')
+const p3 = new Person('poco','5g')
+// console.log(p3.getFullname());
+
 // console.log(p1);
 // console.log(p2);
 // console.log(p1.getFullname());
 // console.log(p2.getFullname());
 
 
+// Person.prototype will be equal to p1.__proto__
+
+// const p1 = new Person() , we just put the prototype from class Person to there....
+
+
+
+// why in JS everything isan object....
+
+// .__proto__.__proto__ everything is object....
+// hence everything is object...
+
+
+// Blueprint -> Prototype
+//Object -> __proto__ (Blueprint)
+/* 
+const p1= new Person()
+p1.__proto__ = Person.prototype //internally this is happening when we do:
+const p1 = new Person();
+//Syntax sugar (syntactical sugar)
+
+
+
+
+
+*/
+
+
+class A {
+    funcInisdeA(){
+
+    }
+}
+
+class B extends A{
+    funInsideB(){
+
+    }
+}
+
+const b = new B();
+// b.funInsideB();
+// b.funInsideA()
+
+
+
+/* 
+
+
+function Name(){
+}
+var x = function(){
+}
+
+const add = (x,y)=>{
+    return x+y
+    }
+const add = (x,y)=> return x+y
+or const add = (x,y)=> x+y
+arr.map(()=>{
+    })
+*/
