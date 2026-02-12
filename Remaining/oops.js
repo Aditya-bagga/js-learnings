7507c9c983813e22804aded00a12835e83562233
Aditya Bagga <aditya.bagga.ynr@gmail.com>
2026-02-12 11:02:07 +0000

    remaining topics of js

---
+++ Remaining/oops.js
@@ -0,0 +1,27 @@
+// JS initially and still prototype based programming language
+// JS  : added synctatically sugar : Object oriented programming language
+
+// object/ instance of class , class which is blueprint 
+
+// object can have a special feature 
+
+// prototype based     // Obejct based
+                
+                //Javascript : class , object , constructor ( we are pretending its oop but we come back at prototype based , it looks like)
+                // (its class based but its just a mask....)
+                // (Is it truly oop based : yeah it supports everything , but behind the scene still prototype )
+
+// prototype : extra properties and functionality in any given datatype....
+
+
+let computer = {
+    cpu : 12
+}
+
+let lenovo = {
+    screen : "HD"
+}
+
+let tomHardware = {}
+
+console.log(computer)
