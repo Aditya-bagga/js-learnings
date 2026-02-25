95379c96ee0478341a23a13add6a5a459bce7993
Aditya Bagga <aditya.bagga.ynr@gmail.com>
2026-02-25 08:04:54 +0000

    revisiting DOM

---
+++ Remaining/DOM.js
@@ -0,0 +1,27 @@
+// DOM and BOM
+
+
+console.log("hello from script")
+
+// web element manipulation
+// window object -> whole screen 
+
+// DOM      BOM      JS
+
+// all elemetns on web : DOM
+
+
+//BOM : screen  , navigator object , location obj 
+
+// everything is inside widnow object (js , DOM , BOM)
+
+
+console.log(window);
+
+// navigator , location these are webAPI , 
+
+//DOM
+// every element is like a tree 
+//HTML-> head , BODY ->h1 , p  -> manipulate these things is DOM
+
+console.log(document.getElementsByTagName("h1"));
